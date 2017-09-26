02484374bf97cc53b0009b096726aebcb2bbac3f
Covina <jabs303@gmail.com>
2017-09-26 08:23:03 -0700

    Section 3. Lecture 19;  Platform generation, Camera lerping

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    // The Player prefab
+    [SerializeField]
+    private GameObject player;
+
+    // The Platform prefab
+    [SerializeField]
+    private GameObject platform;
+
+
+    // Coordinate boundaries for creating the platforms
+    private float minX = -2.3f;
+    private float maxX = 2.3f;
+    private float minY = -4.7f;
+    private float maxY = -3.7f;
+
+
+    private bool lerpCamera;
+    private float lerpTime = 1.5f;
+    private float lerpX;
+
+
+    // 
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    // Create static instance of GameManager
+    private void MakeInstance()
+    {
+        if (instance == null) {
+            instance = this;
+        }
+        
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        CreateInitialPlatforms();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Are we moving the camera?
+        if (lerpCamera)
+        {
+            LerpTheCamera();
+        }
+
+
+	}
+
+
+    void CreateInitialPlatforms()
+    {
+        // Generte starting location of first platform, clamp the X a bit
+        Vector3 temp = new Vector3(Random.Range(minX, minX + 1.2f), Random.Range(minY, maxY), 0f);
+
+        // Spawn the platform
+        Instantiate(platform, temp, Quaternion.identity);
+
+        // Generate location for player, move Y coord up 2 units
+        temp.y += 2f;
+
+        // Spawn player on top of this starting platform
+        Instantiate(player, temp, Quaternion.identity);
+
+
+        // Generate location for second platform
+        temp = new Vector3(Random.Range(maxX, maxX - 1.2f), Random.Range(minY, maxY), 0f);
+
+        // Spawn the second platform
+        Instantiate(platform, temp, Quaternion.identity);
+
+
+    }
+
+    // Create a new platform on which the player should jump
+    private void CreateNewPlatform()
+    {
+
+        // Get the current X pos of the camera
+        float cameraX = Camera.main.transform.position.x;
+
+        // double screen width then add the camera X pos to find its location in the new view
+        float newMaxX = (maxX * 2) + cameraX;
+
+        // Create the new platform
+        Instantiate(platform, new Vector3 (Random.Range(newMaxX, newMaxX - 1.2f), Random.Range(maxY, maxY - 1.2f), 0f), Quaternion.identity);
+
+    }
+
+    // Camera movement function
+    private void LerpTheCamera()
+    {
+        // Get camera's current position   
+        float x = Camera.main.transform.position.x;
+
+        // calculate the nwe value of X Pos
+        x = Mathf.Lerp(x, lerpX, lerpTime * Time.deltaTime);
+
+        // Set new camera X position
+        Camera.main.transform.position = new Vector3(x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+
+        // if camera has moved almost the entire way, disable the movement
+        if(Camera.main.transform.position.x >= (lerpX - 0.07f))
+        {
+            // Turn off lerping   
+            lerpCamera = false;
+
+        }
+
+    }
+
+    // Create the platform and move the camera.
+    public void CreateNewPlatformAndLerp(float lerpPosition)
+    {
+        // Calculate new final X position
+        lerpX = lerpPosition + maxX;
+
+        // turn on camera movement
+        lerpCamera = true;
+
+        // spawn the new platform
+        CreateNewPlatform();
+
+
+    }
+
+
+
+}
--- Assets/Scripts/Player Scripts/PlayerJumpScript.cs
+++ Assets/Scripts/Player Scripts/PlayerJumpScript.cs
@@ -23,9 +23,37 @@ public class PlayerJumpScript : MonoBehaviour {
 
     void Awake()
     {
+        // create singleton
+        MakeInstance();
 
+        // Init all the game object and component references
+        Initialize();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
 
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyPower();
+    }
+
+    // Make all the initial game object connections
+    private void Initialize()
+    {
+        // get the Rigidbody attached to the Player game object
+        myBody = GetComponent<Rigidbody2D>();
+
+        // get the Animator on the Player game object
+        anim = GetComponent<Animator>();
+
+    }
+
 
     // Create static instance of the class
     void MakeInstance()
@@ -39,32 +67,88 @@ public class PlayerJumpScript : MonoBehaviour {
     }
 
 
+    private void ApplyPower()
+    {
+        // Check if setPower is TRUE
+        if (setPower)
+        {
+            Debug.Log("We are setting the power");
+
+            // set the power values
+            forceX += thresholdX * Time.deltaTime;
+            forceY += thresholdY * Time.deltaTime;
+
+            // Cap the ForceX
+            if (forceX >= 6.5f)
+            {
+                forceX = 6.5f;
+            }
+
+            // Cap the ForceY
+            if (forceY >= 13.5f)
+            {
+                forceY = 13.5f;
+            }
+        }
+
+    }
+
     public void SetPower(bool setPower)
     {
         // set the class variable to the passed argument value   
         this.setPower = setPower;
 
-        // Check if setPower is TRUE
-        if(setPower)
+        // if we are not setting the power, jump!
+        if (!setPower)
         {
-            Debug.Log("We are setting the power");
-        } else {
-
-            // do something
-            Debug.Log("We are NOT setting the power");
-
+            Jump();
         }
 
     }
 
+    // Jump function
+    private void Jump()
+    {
+        // make the player jump
+        myBody.velocity = new Vector2(forceX, forceY);
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // reset the force values to zero
+        forceX = forceY = 0f;
+
+        // set didJump status
+        didJump = true;
+
+
+    }
+
+    // Check for collision only if previously jumped
+    void OnTriggerEnter2D(Collider2D target)
+    {
+
+        if (didJump)
+        {
+            // reset the value
+            didJump = false;
+
+            // Check to see if player landed on the platform
+            if (target.tag == "Platform")
+            {
+
+                Debug.Log("Player collided with the platform");
+
+                if(GameManager.instance != null) {
+
+                    // move the camera to the X position of the second platform.
+                    GameManager.instance.CreateNewPlatformAndLerp(target.transform.position.x);
+
+                }
+                
+
+
+            }
+
+        }
+    }
+
+
 }
